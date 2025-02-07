42b5145d34e87e21615ea5f280c2dba923f2537b
ankit-olatech <ankit.j@olatechs.com>
2025-02-07 12:46:33 +0530

    -

--- youtube_analysis/youtube_analysis/settings.py
+++ youtube_analysis/youtube_analysis/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'youtube_analysis',
-    'rest_framework'
+    'rest_framework',
+    'video_analysis'
 ]
 
 MIDDLEWARE = [
--- youtube_analysis/youtube_analysis/urls.py
+++ youtube_analysis/youtube_analysis/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('api/', include('video_analysis.urls')),
+    path('api/', include('video_analysis.urls')),
 ]
