633847f3a57581ef0981f9aa9e5e72f8b4c880d3
ankit-olatech <ankit.j@olatechs.com>
2025-02-07 14:33:58 +0530

    test_phase1_api_done

--- youtube_analysis/video_analysis/models.py
+++ youtube_analysis/video_analysis/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Video(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="NA")
     description = models.TextField(blank=True, null=True)
     upload_date = models.DateTimeField(blank=True, null=True)
-    url = models.URLField(max_length=500)
+    url = models.URLField(max_length=500, default="NA")
     file = models.FileField(upload_to='videos/', blank=True, null=True)  # For manual upload
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
--- youtube_analysis/video_analysis/views.py
+++ youtube_analysis/video_analysis/views.py
@@ -12,4 +12,4 @@ class VideoUploadView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=400)  
